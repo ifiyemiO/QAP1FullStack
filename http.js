cc2c69fcab3fa28ad78ba2b5d16cc4d8c60498ef
Ifiyemi Owi <ifiyemi.owi@keyin.com>
2024-05-23 16:39:06 -0230

    http

--- http.js
+++ http.js
@@ -1,24 +1,31 @@
 // node [http]
-// http is a core global object in node.js.
+// http is a core global object in node.js that manages and runs a server.
 // It is also a built-in module which allows transfer of data over the Hyper Text Transfer protocol (HTTP).
 // it is used in the creation of server (listens for request and send responses)
 // and client (send request to server).
 
 // require('node:http') is a must to use HTTP server and client.
 
-// Sample code
+// Sample code to create a server
+// reference for library http
 const http = require("http");
 const hostname = "127.0.0.1";
 const port = 5500;
 
-const server = http.createServer((req, res) => {
-  console.log(req);
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("A Beautiful Sunny Day for taking a Walk around the Pond.");
+// create server using request by client and response by server
+const server = http.createServer((request, response) => {
+  console.log(request);
+  response.statusCode = 200;
+
+  // type of data to be sent
+  response.setHeader("Content-Type", "text/plain");
+  response.end("A Beautiful Sunny Day for taking a Walk around the Pond.");
 });
 
+// terminal side
 console.log("It is a sunny Day");
+
+// server side
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
